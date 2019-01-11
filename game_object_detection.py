c521b9cfa50dad3ee9b86d4e89163c6a2b30b8a4
Simon Sørensen <fynboen1999@gmail.com>
2019-01-12 00:44:36 +0100

    Add files via upload

---
+++ game_object_detection.py
@@ -0,0 +1,21 @@
+# Import OpenCV (cv2) & numpy as np
+import cv2
+import numpy as np
+# Pillow (PIL) lets us use ImageGrab
+from PIL import ImageGrab
+# Infinite loop
+while True:
+    # Our game screen in a fixed bounding box
+    # OpenCV only accepts images in a certain format, images as arrays
+    # We convert the images grabbed from our screen to an array using numpy
+    game_screen = np.array(ImageGrab.grab(bbox=(0, 0, 800, 600)))
+    # Convert the game screen to gray for faster processing of object detection
+    game_gray = cv2.cvtColor(game_screen, cv2.COLOR_BGR2GRAY)
+    # Show the screen and convert the colors from BGR to RGB
+    cv2.imshow('Game Window', cv2.cvtColor(game_screen, cv2.COLOR_BGR2RGB))
+    # Exit screen feed by button press
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        break
+    
+# Destroy all when done
+cv2.destroyAllWindows()
---
+++ video_object_detection_with_picam.py
@@ -0,0 +1,53 @@
+# 27-12-2018
+# Simon S. Sørensen
+# Detect a specific object within a live video feed from a PiCam
+
+# Import OpenCV (cv2) & PiCamera modules, use pip to install modules
+import cv2
+from time import sleep
+from picamera import PiCamera
+from picamera.array import PiRGBArray
+
+# Our object cascade from a given path
+object_cascade = cv2.CascadeClassifier('./cascades/haarcascade_frontalface_default.xml')
+
+# PiCam settings
+camera = PiCamera()
+camera.resolution(640, 360)
+camera.framerate(30)
+
+# Video capture
+video_capture = PiRGBArray(camera, (640, 360))
+
+# Let the camera warm up
+sleep(0.1)
+
+# Start video frame capture
+for still in camera.capture_continous(video_capture, format='bgr', use_video_port=True):
+    # Frame as an array
+    image = still.array()
+    # Convert image to gray scale
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # Detecting facial features
+    faces = object_cascade.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(35, 35), flags=cv2.cv.CV_HAAR_SCALE_IMAGE)
+    # Drawing a rectangle around the detected face
+    for(x, y, w, h) in faces:
+        # Draw the rectangle
+        cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        # Text placeholder
+        cv2.rectangle(image, (x, y), (x + 48, y + 20), (0, 255, 0), cv2.FILLED)
+        # Draw text
+        cv2.putText(image, 'Face', (x + 5, y + 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+
+    # Show the captured frames
+    cv2.imshow('PiCam Live video feed', image)
+
+    # Clear the video capture
+    video_capture.truncate(0)
+
+    # Exit video feed by button press (q in this case)
+    if cv2.waitKey(30) & 0xFF == ord('q'):
+        break
+
+# Destroy all when done
+cv2.destroyAllWindows()
